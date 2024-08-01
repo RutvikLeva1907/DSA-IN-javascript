f2d8b30a8fd34935cbac33e00f61a5a0dad77cb8
RutvikLeva1907 <159470977+RutvikLeva1907@users.noreply.github.com>
2024-08-01 15:39:29 +0530

    loop

---
+++ lecture-2/qut-2/index.js
@@ -0,0 +1,17 @@
+
+
+
+const muvzeero=()=>{
+    let i=0
+    let d=0;
+    let r=arr.length
+    while (i<r) {
+        if (arr[i]==0) {
+
+            
+        }
+        
+    }
+}
+
+let arr=[0,1,2,0,4]
